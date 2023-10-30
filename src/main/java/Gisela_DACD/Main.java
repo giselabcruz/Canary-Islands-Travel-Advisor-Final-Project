c12b39bf830a2ea8e61284b9007094eaa894b94d
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-10-30 13:02:58 +0000

    Implementation of encapsulated logic for get and save weather information.

--- src/main/java/Gisela_DACD/Main.java
+++ src/main/java/Gisela_DACD/Main.java
@@ -2,15 +2,7 @@ package Gisela_DACD;
 
 import Gisela_DACD.P1Model.*;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import java.io.IOException;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
 
 public class Main {
@@ -19,9 +11,8 @@ public class Main {
         String apiKey = dotenv.get("OPEN_WEATHER_APIKEY");
         ArrayList<Island> islands = IslandProvider.initializeIslands();
 
-        for (Island island : islands) {
-            String url = String.format("https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric&appid=%s",
-                            island.getLat(), island.getLongitude(),apiKey);
+        WeatherDataFetcher dataFetcher = new WeatherDataFetcher(apiKey);
+        WeatherDataRepository dataSaver = new WeatherDataSaverInSQLite();
 
             CloseableHttpClient httpclient = HttpClients.createDefault();
             HttpGet httpGet = new HttpGet(url);
