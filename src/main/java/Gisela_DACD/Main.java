0613416f91cf6b9db7ed680fa054689f018e99b2
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-10-30 13:23:15 +0000

    TEST CODE: Update task every 5 minutes.

--- src/main/java/Gisela_DACD/Main.java
+++ src/main/java/Gisela_DACD/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
         Timer timer = new Timer();
         WeatherUpdaterTask updaterTask = new WeatherUpdaterTask(dataFetcher, dataSaver, islands);
-        timer.schedule(updaterTask, 0, 6 * 60 * 60 * 1000);
+        timer.schedule(updaterTask, 0, 300_000); //5 minutes in milliseconds
+        //timer.schedule(updaterTask, 0, 6 * 60 * 60 * 1000);
     }
 }
