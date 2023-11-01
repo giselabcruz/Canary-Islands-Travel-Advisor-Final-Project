8d23e16d18a4a8df81123ad1a255c8c0cbc03688
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-11-01 20:53:33 +0000

    Changed property.

--- src/main/java/Gisela_DACD/P1Model/Rain.java
+++ src/main/java/Gisela_DACD/P1Model/Rain.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Rain {
     private double hour;
 
-    @JsonProperty("1h")
+    @JsonProperty("3h")
     public double getHour() {
         return hour;
     }
 
-    @JsonProperty("1h")
+    @JsonProperty("3h")
     public void setHour(double hour) {
         this.hour = hour;
     }
