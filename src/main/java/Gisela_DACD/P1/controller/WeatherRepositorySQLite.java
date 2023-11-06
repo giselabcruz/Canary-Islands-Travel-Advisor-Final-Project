4736978ca0ea78af118520d1876c4c50d5f8a0f1
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-11-06 09:30:33 +0000

    Refactor: implemented try-with-resources.

--- src/main/java/Gisela_DACD/P1/controller/WeatherRepositorySQLite.java
+++ src/main/java/Gisela_DACD/P1/controller/WeatherRepositorySQLite.java
@@ -19,24 +19,19 @@ public class WeatherRepositorySQLite implements WeatherRepository {
     public void saveWeatherData(Location location, Weather weather) throws SQLException {
 
         String islandName = location.getName();
+        String insertSQL = "INSERT INTO table_" + islandName + "_weather (datetime, temperature, precipitation, " +
+                "humidity, clouds, wind_speed) VALUES (?, ?, ?, ?, ?, ?)";
 
-        try {
-            String insertSQL = "INSERT INTO table_" + islandName + "_weather (datetime, temperature, precipitation, " +
-                    "humidity, clouds, wind_speed) VALUES (?, ?, ?, ?, ?, ?)";
-
-            PreparedStatement preparedStatement = connection.prepareStatement(insertSQL);
-
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)){
             preparedStatement.setString(1, weather.getTs().toString());
             preparedStatement.setDouble(2, weather.getTemperature());
             preparedStatement.setDouble(3, weather.getPrecipitation());
             preparedStatement.setDouble(4, weather.getHumidity());
             preparedStatement.setDouble(5, weather.getClouds());
             preparedStatement.setDouble(6, weather.getWindSpeed());
-
             preparedStatement.executeUpdate();
-            System.out.println("Weather data inserted into the corresponding table.");
 
-            preparedStatement.close();
+            System.out.println("Weather data inserted into the corresponding table.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
