3071545404182ed72d8edf63bebe4317c8564ae6
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-11-08 23:33:36 +0000

    Implemented timer.scheduleAtFixedRate to ensure task is executed at the specified rate, even if previous executions took longer.

--- src/main/java/org/gisela/dacd/p1/Main.java
+++ src/main/java/org/gisela/dacd/p1/Main.java
@@ -12,25 +12,17 @@ public class Main {
         Dotenv dotenv = Dotenv.configure().load();
         String apiKey = dotenv.get("OPEN_WEATHER_APIKEY");
         ArrayList<Location> locations = LocationSupplier.initializeIslands();
-
         OpenWeatherProvider weatherOpenWeatherApiQuery = new OpenWeatherProvider(apiKey);
         SQLiteConnector connector = new SQLiteConnector();
         connector.createOrUpdateTable();
         WeatherRepository weatherRepository = new WeatherRepositorySQLite(connector.getConnection());
         //TODO: get connection can't be here. Improve encapsulating WeatherRepositorySQLite.
-
         WeatherController weatherController = new WeatherController(weatherOpenWeatherApiQuery, weatherRepository, locations);
-
         Timer timer = new Timer();
         WeatherPeriodicTask updaterTask = new WeatherPeriodicTask(weatherController, "jdbc:sqlite:canary_islands_weather.db", weatherOpenWeatherApiQuery, locations);
-        timer.schedule(updaterTask, 0, 6 * 60 * 60 * 1000);
-        //TODO: Implement timer.scheduleAtFixedRate to maintain a consistent time interval between task executions.
-        // It attempts to ensure that the task is executed at the specified rate, even if previous executions took longer.
+        timer.scheduleAtFixedRate(updaterTask, 0, 6 * 60 * 60 * 1000);
         connector.closeConnection();
-
-
         //TODO: Delete spaces in all code.
-
         //TODO: change var environment to args.
     }
 }
