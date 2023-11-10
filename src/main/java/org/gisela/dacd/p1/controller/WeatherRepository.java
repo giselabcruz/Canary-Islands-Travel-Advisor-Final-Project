d07c0230bb8ba996d90d437e29de4002e44a49c0
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-11-10 22:00:04 +0000

    Completed TODO.

--- src/main/java/org/gisela/dacd/p1/controller/WeatherRepository.java
+++ src/main/java/org/gisela/dacd/p1/controller/WeatherRepository.java
@@ -3,9 +3,7 @@ package org.gisela.dacd.p1.controller;
 import org.gisela.dacd.p1.model.Location;
 import org.gisela.dacd.p1.model.Weather;
 
-import java.sql.SQLException;
 
 public interface WeatherRepository {
-    void saveWeatherData(Location location, Weather weather) throws SQLException;
-    //TODO: refactor code in WeatherRepositorySQLite
+    void saveWeatherData(Location location, Weather weather);
 }
