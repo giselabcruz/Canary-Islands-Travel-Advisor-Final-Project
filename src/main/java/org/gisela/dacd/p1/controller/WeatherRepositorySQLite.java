d10182b9c198c8fa83f67d1e2cf2d256b1176e88
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-11-10 21:40:08 +0000

    Class converted to a record.

--- src/main/java/org/gisela/dacd/p1/controller/WeatherRepositorySQLite.java
+++ src/main/java/org/gisela/dacd/p1/controller/WeatherRepositorySQLite.java
@@ -8,18 +8,14 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
-public class WeatherRepositorySQLite implements WeatherRepository {
-    public final Connection connection;
-    public WeatherRepositorySQLite(Connection connection) {
-        this.connection = connection;
-    }
+public record WeatherRepositorySQLite(Connection connection) implements WeatherRepository {
 
     @Override
     public void saveWeatherData(Location location, Weather weather) {
         String islandName = location.getName();
         String insertSQL = "INSERT INTO table_" + islandName + "_weather (datetime, temperature, precipitation, " +
                 "humidity, clouds, wind_speed) VALUES (?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)){
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
             preparedStatement.setString(1, weather.getTs().toString());
             preparedStatement.setDouble(2, weather.getTemperature());
             preparedStatement.setDouble(3, weather.getPrecipitation());
