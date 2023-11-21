3e75c44a622951e38057ca8e9990d75d2ea7f12a
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-11-21 21:32:51 +0000

    Created class WeatherEvent and created its instance in WeatherController.

--- predictionProvider/src/main/java/org/gisela/dacd/publisher/controller/WeatherController.java
+++ predictionProvider/src/main/java/org/gisela/dacd/publisher/controller/WeatherController.java
@@ -3,8 +3,11 @@ package org.gisela.dacd.publisher.controller;
 import org.gisela.dacd.publisher.model.Location;
 import org.gisela.dacd.publisher.model.Weather;
 import org.gisela.dacd.publisher.model.WeatherRepository;
+import org.gisela.dacd.publisher.model.events.WeatherEvent;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class WeatherController {
@@ -23,6 +26,9 @@ public class WeatherController {
             List<Weather> weatherlist = openWeatherProvider.getWeatherData(location);
             for (Weather weather : weatherlist) {
                 weatherRepository.saveWeatherData(location, weather);
+                WeatherEvent event = new WeatherEvent(new Date().toInstant(), "prediction-provider", weather.getTs(),
+                        weather.getLocation(), weather.getHumidity(), weather.getTemperature(), weather.getPrecipitation(),
+                        weather.getClouds(), weather.getWindSpeed());
             }
         }
     }
---
+++ predictionProvider/src/main/java/org/gisela/dacd/publisher/model/events/WeatherEvent.java
@@ -0,0 +1,65 @@
+package org.gisela.dacd.publisher.model.events;
+
+import org.gisela.dacd.publisher.model.Location;
+import java.time.Instant;
+
+public class WeatherEvent {
+    private final Instant ts;
+    private final String ss;
+    private final Instant predictionTime;
+    private final Location location;
+    private final double humidity;
+    private final double temperature;
+    private final double precipitation;
+    private final double clouds;
+    private final double windSpeed;
+
+    public WeatherEvent(Instant ts, String ss, Instant predictionTime, Location location,
+                        double humidity, double temperature, double precipitation, double clouds, double windSpeed) {
+        this.ts = ts;
+        this.ss = ss;
+        this.predictionTime = predictionTime;
+        this.location = location;
+        this.humidity = humidity;
+        this.temperature = temperature;
+        this.precipitation = precipitation;
+        this.clouds = clouds;
+        this.windSpeed = windSpeed;
+    }
+
+    public Instant getTs() {
+        return ts;
+    }
+
+    public String getSs() {
+        return ss;
+    }
+
+    public Instant getPredictionTime() {
+        return predictionTime;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getPrecipitation() {
+        return precipitation;
+    }
+
+    public double getClouds() {
+        return clouds;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+}
