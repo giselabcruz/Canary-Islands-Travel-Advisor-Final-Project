6dc059bd5647a729ae0a7cc3e536abecf08e3dbb
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-11-28 22:25:00 +0000

    Deleted final constants and created Setters.

--- predictionProvider/src/main/java/org/gisela/dacd/provider/model/events/WeatherEvent.java
+++ predictionProvider/src/main/java/org/gisela/dacd/provider/model/events/WeatherEvent.java
@@ -4,15 +4,15 @@ import org.gisela.dacd.provider.model.Location;
 import java.time.Instant;
 
 public class WeatherEvent {
-    private final Instant ts;
-    private final String ss;
-    private final Instant predictionTime;
-    private final Location location;
-    private final double humidity;
-    private final double temperature;
-    private final double precipitation;
-    private final double clouds;
-    private final double windSpeed;
+    private Instant ts;
+    private String ss;
+    private Instant predictionTime;
+    private Location location;
+    private double humidity;
+    private double temperature;
+    private double precipitation;
+    private double clouds;
+    private double windSpeed;
 
     public WeatherEvent(Instant ts, String ss, Instant predictionTime, Location location,
                         double humidity, double temperature, double precipitation, double clouds, double windSpeed) {
@@ -31,35 +31,71 @@ public class WeatherEvent {
         return ts;
     }
 
+    public void setTs(Instant ts) {
+        this.ts = ts;
+    }
+
     public String getSs() {
         return ss;
+    }
+
+    public void setSs(String ss) {
+        this.ss = ss;
     }
 
     public Instant getPredictionTime() {
         return predictionTime;
     }
 
+    public void setPredictionTime(Instant predictionTime) {
+        this.predictionTime = predictionTime;
+    }
+
     public Location getLocation() {
         return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 
     public double getHumidity() {
         return humidity;
     }
 
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
     public double getTemperature() {
         return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
     }
 
     public double getPrecipitation() {
         return precipitation;
     }
 
+    public void setPrecipitation(double precipitation) {
+        this.precipitation = precipitation;
+    }
+
     public double getClouds() {
         return clouds;
     }
 
+    public void setClouds(double clouds) {
+        this.clouds = clouds;
+    }
+
     public double getWindSpeed() {
         return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
     }
 }
