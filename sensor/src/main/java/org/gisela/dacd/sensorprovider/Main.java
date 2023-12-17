8bc74771efb337dc90ebf7b4c7b5c5c80c51874c
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-12-17 20:34:49 +0000

    Implemented Main

--- sensor/src/main/java/org/gisela/dacd/sensorprovider/Main.java
+++ sensor/src/main/java/org/gisela/dacd/sensorprovider/Main.java
@@ -1,9 +1,19 @@
 package org.gisela.dacd.sensorprovider;
 
+import org.gisela.dacd.sensorprovider.application.HotelPeriodicTask;
+import org.gisela.dacd.sensorprovider.application.HotelSupplier;
+import org.gisela.dacd.sensorprovider.domain.Hotel;
+import org.gisela.dacd.sensorprovider.infrastructure.XoteloProvider;
+import java.util.ArrayList;
+import java.util.Timer;
+
 public class Main {
 
     public static void main(String[] args){
-        String apiKey = args[1];
+        ArrayList<Hotel> hotels = HotelSupplier.canaryIslandsHotels();
+        XoteloProvider xoteloProvider = new XoteloProvider();
+        Timer timer = new Timer();
+        HotelPeriodicTask updaterTask = new HotelPeriodicTask(xoteloProvider, hotels);
+        timer.scheduleAtFixedRate(updaterTask, 0, 6 * 60 * 60 * 1000);
     }
-
 }
