1918b63566a483a13b5b1028898d84c0dd85163f
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2023-12-17 20:31:44 +0000

    Implemented new method

--- sensor/src/main/java/org/gisela/dacd/sensorprovider/application/HotelProvider.java
+++ sensor/src/main/java/org/gisela/dacd/sensorprovider/application/HotelProvider.java
@@ -1,11 +1,9 @@
 package org.gisela.dacd.sensorprovider.application;
 
 import org.gisela.dacd.sensorprovider.domain.Hotel;
-import org.gisela.dacd.sensorprovider.domain.Location;
 import java.util.List;
 
 public interface HotelProvider {
-
-    List<Hotel> getHotelData(Location location);
-
+    List<Hotel> getHotelRates(String hotelKey);
+    List<Hotel> getHotelHeatmap(String hotelKey);
 }
