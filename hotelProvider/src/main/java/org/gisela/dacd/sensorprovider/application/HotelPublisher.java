c66e2aa73ea59e214a22dad4d4acc4ce84b426cc
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2024-01-03 20:32:01 +0000

    Refactored

--- hotelProvider/src/main/java/org/gisela/dacd/sensorprovider/application/HotelPublisher.java
+++ hotelProvider/src/main/java/org/gisela/dacd/sensorprovider/application/HotelPublisher.java
@@ -3,9 +3,7 @@ package org.gisela.dacd.sensorprovider.application;
 import org.gisela.dacd.sensorprovider.domain.Hotel;
 
 public interface HotelPublisher {
-
     void start();
     void publish (Hotel event);
     void close();
-
 }
