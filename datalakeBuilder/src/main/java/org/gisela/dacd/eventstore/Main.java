64291196a33cd62acd42b4d0df53c66835a0b4b0
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2024-01-03 20:20:04 +0000

    Created new topic

--- datalakeBuilder/src/main/java/org/gisela/dacd/eventstore/Main.java
+++ datalakeBuilder/src/main/java/org/gisela/dacd/eventstore/Main.java
@@ -7,13 +7,15 @@ import org.gisela.dacd.eventstore.infrastructure.SubscriberActiveMQ;
 public class Main {
     private static final String BROKER_URL = "tcp://localhost:61616";
     private static final String CLIENT_ID = "Gisela";
-    private static final String TOPIC_NAME = "prediction.Weather";
+    private static final String PREDICTION_WEATHER_TOPIC = "prediction.Weather";
+    private static final String HOTEL_TOPIC = "sensor.Hotel";
     private static final String SUBSCRIBER_ID = "Gisela";
 
     public static void main(String[] args) {
         EventStoreManager eventStoreManager = new EventStoreManager();
         Subscriber subscriber = new SubscriberActiveMQ(BROKER_URL,CLIENT_ID, SUBSCRIBER_ID, eventStoreManager);
         subscriber.start();
-        subscriber.subscribe(TOPIC_NAME);
+        subscriber.subscribe(PREDICTION_WEATHER_TOPIC);
+        subscriber.subscribe(HOTEL_TOPIC);
     }
 }
