6f65b332ebfbbbdf1399eece9d6e1e4bdc07993e
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2024-01-03 20:18:03 +0000

    Created InstantTypeAdapter for hotelProvider

---
+++ hotelProvider/src/main/java/org/gisela/dacd/sensorprovider/utils/InstantTypeAdapter.java
@@ -0,0 +1,18 @@
+package org.gisela.dacd.sensorprovider.utils;
+
+import com.google.gson.*;
+import java.lang.reflect.Type;
+import java.time.Instant;
+
+public class InstantTypeAdapter implements JsonSerializer<Instant>, JsonDeserializer<Instant> {
+    @Override
+    public JsonElement serialize(Instant instant, Type srcType,
+                                 JsonSerializationContext context) {
+        return new JsonPrimitive(instant.toString());
+    }
+    @Override
+    public Instant deserialize(JsonElement json, Type typeOfT,
+                               JsonDeserializationContext context) throws JsonParseException {
+        return Instant.parse(json.getAsString());
+    }
+}
