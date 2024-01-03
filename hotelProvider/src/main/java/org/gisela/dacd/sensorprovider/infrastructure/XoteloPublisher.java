e56e4f5220e8d4a4757866883a95efbad5cee51f
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2024-01-03 20:26:46 +0000

    Implemented publish method

--- hotelProvider/src/main/java/org/gisela/dacd/sensorprovider/infrastructure/XoteloPublisher.java
+++ hotelProvider/src/main/java/org/gisela/dacd/sensorprovider/infrastructure/XoteloPublisher.java
@@ -1,10 +1,13 @@
 package org.gisela.dacd.sensorprovider.infrastructure;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.gisela.dacd.sensorprovider.application.HotelPublisher;
 import org.gisela.dacd.sensorprovider.domain.Hotel;
+import org.gisela.dacd.sensorprovider.utils.InstantTypeAdapter;
 import javax.jms.*;
-
+import java.time.Instant;
 
 public class XoteloPublisher implements HotelPublisher {
 
@@ -17,12 +20,26 @@ public class XoteloPublisher implements HotelPublisher {
         try {
             connection = connectionFactory.createConnection();
             connection.start();
-           } catch (JMSException e) {
+        } catch (JMSException e) {
             throw new RuntimeException(e);
         }
     }
     @Override
     public void publish(Hotel event) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Instant.class, new InstantTypeAdapter())
+                .create();
+        String jsonEvent = gson.toJson(event);
+        try {
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createTopic("sensor.Hotel");
+            MessageProducer producer = session.createProducer(destination);
+            TextMessage message = session.createTextMessage(jsonEvent);
+            producer.send(message);
+            System.out.println("JCG printing@@ '" + message.getText() + "'");
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
