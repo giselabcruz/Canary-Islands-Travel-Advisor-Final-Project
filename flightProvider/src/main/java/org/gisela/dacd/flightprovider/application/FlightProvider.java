0b83a19bdeed7ddfbf2edd2bf7a9342b19e0f618
MissWond3r <giselaisabelbelmontecruz@gmail.com>
2024-01-09 19:44:24 +0000

    Deleted FlightProvider

--- flightProvider/src/main/java/org/gisela/dacd/flightprovider/application/FlightProvider.java
+++
@@ -1,9 +0,0 @@
-package org.gisela.dacd.flightprovider.application;
-
-import org.gisela.dacd.flightprovider.domain.Flight;
-import org.gisela.dacd.flightprovider.domain.Location;
-import java.util.List;
-
-public interface FlightProvider {
-    List<Flight> getWeatherData(Location location);
-}
--- flightProvider/src/main/java/org/gisela/dacd/flightprovider/application/Publisher.java
+++
@@ -1,9 +0,0 @@
-package org.gisela.dacd.flightprovider.application;
-
-import org.gisela.dacd.flightprovider.domain.Flight;
-
-public interface Publisher {
-    void start();
-    void publish (Flight event);
-    void close();
-}
